b6b43855baf3c3a8481569ee10eec09d4e717896
jinxiu89 <jinxiu89@163.com>
2020-05-27 18:09:46 +0800

    测试文件夹权限

---
+++ app/admin/base.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @Create by PhpStorm
+ * @author:jinxiu89@163.com
+ * @Create Date:2020/5/27 17:59
+ * @User: admin
+ * @Current File : base.php
+ * @格言：溪涧岂能留得住，终归大海做波涛 --李忱
+ * @格言： 我的内心因看见大海而波涛汹涌
+ **/
+
+class base
+{
+
+}
