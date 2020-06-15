8b7e83b96a49c3e411668519bc458cafd2963ec1
jinxiu89 <jinxiu89@163.com>
2020-06-15 09:22:17 +0800

    新增商品 功能细节

--- public/static/admin/js/common.js
+++ public/static/admin/js/common.js
@@ -10,13 +10,20 @@ function del(url) {
             dataType: "json",
             success: function (result) {
                 if (result.status === 1) {
-                    layer.msg(result.message, {icon: 1, time: 600},function () {
+                    layer.msg(result.message, {icon: 1, time: 600}, function () {
                         location.replace(location.href)
                     });
                 } else {
                     layer.msg(result.message, {icon: 5, time: 2000});
                 }
             }
-        },'JSON')
+        }, 'JSON')
     })
+}
+
+function insert(id, key) {
+    let index = parent.layer.getFrameIndex(window.name);
+    parent.$('#' + id + '').attr('src', key);
+    parent.$('#' +id+'').siblings('#thumbnail').val(key);
+    parent.layer.close(index);
 }
