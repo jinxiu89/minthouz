0241f0e5bcdfeb5126a50746ed5dc56d548e9465
jinxiu89 <jinxiu89@163.com>
2020-07-06 15:18:36 +0800

    前端模板配置文件

---
+++ app/frontend/config/view.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * @Create by PhpStorm
+ * @author:jinxiu89@163.com
+ * @Create Date:2020/7/6 15:15
+ * @User: admin
+ * @Current File : view.php
+ * @格言：溪涧岂能留得住，终归大海做波涛 --李忱
+ * @格言： 我的内心因看见大海而波涛汹涌
+ **/
+return [
+    // 模板引擎类型使用Think
+    'type'          => 'Think',
+    // 默认模板渲染规则 1 解析为小写+下划线 2 全部转换小写 3 保持操作方法
+    'auto_rule'     => 1,
+    // 模板目录名
+    'view_dir_name' => 'view',
+    // 模板后缀
+    'view_suffix'   => 'html',
+    // 模板文件名分隔符
+    'view_depr'     => DIRECTORY_SEPARATOR,
+    // 模板引擎普通标签开始标记
+    'tpl_begin'     => '{',
+    // 模板引擎普通标签结束标记
+    'tpl_end'       => '}',
+    // 标签库标签开始标记
+    'taglib_begin'  => '{',
+    // 标签库标签结束标记
+    'taglib_end'    => '}',
+    'tpl_replace_string'=>[
+        '{{frontend}}'     =>'/static/frontend'
+    ],
+];
