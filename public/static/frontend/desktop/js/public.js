e03551e37bf2afb02108555940da8b3d7466732b
Alan <1367428189@qq.com>
2020-10-08 18:22:19 +0800

    导航交互效果调整 - 完成

--- public/static/frontend/desktop/js/public.js
+++ public/static/frontend/desktop/js/public.js
@@ -1,11 +1,11 @@
-function gNotice () {
+function gNotice() {
     var close = $('#g-notice .close');
     close.click(function () {
         $(this).parents('#g-notice').slideUp()
     })
 }
 
-function menuHover () {
+/*function menuHover () {
     var nav = $('#nav'),
         navItem = $('#menu-list .menu-item');
     navItem.hover(function () {
@@ -15,7 +15,7 @@ function menuHover () {
         nav.removeClass('navActive');
         $(this).children('.item-children').css('display', 'none');
     })
-}
+}*/
 
 /*function newMenu () {
     var nav = $('#nav'),
@@ -43,7 +43,39 @@ function menuHover () {
     });
 }*/
 
-function gTop () {
+// 2020.10.08 添加导航交互样式 - 新版
+function handelMenuHover() {
+    var nav = $('#nav'),
+        navItem = $('#menu-list .menu-item');
+    nav.mouseleave(function () {
+        nav.removeClass('navActive');
+    });
+    nav.mouseleave(function () {
+        $(this).find('.item-children').slideUp(200);
+        $(this).find('.menu-item').removeClass('menu-item-active');
+    });
+    navItem.mouseenter(function () {
+        var _this = this,
+            _thisItemDom = $(_this).children('.item-children');
+        nav.addClass('navActive');
+        $(_this).siblings('li').removeClass('menu-item-active');
+        $(_this).addClass('menu-item-active');
+        navItem.toArray().forEach(function (item) {
+            var itemDom = $(item).children('.item-children');
+            if (itemDom.css('display') === 'block') {
+                _thisItemDom.css('display', 'block');
+                if (_thisItemDom.length === 0) {
+                    $(_this).siblings('li').children('.item-children').slideUp(200)
+                } else {
+                    $(_this).siblings('li').children('.item-children').css('display', 'none');
+                }
+            }
+        });
+        $(_this).children('.item-children').slideDown(200)
+    });
+}
+
+function gTop() {
     var g_top = $("#g-top");
     g_top.click(function () {
         $("html,body").animate({"scrollTop": 0}, 300);
@@ -54,9 +86,11 @@ $(function () {
 
     gNotice();
 
-    menuHover();
+    // menuHover();
 
     // newMenu();
+
+    handelMenuHover()
 
     gTop();
 
