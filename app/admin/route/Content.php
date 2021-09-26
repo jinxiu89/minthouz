9f2e87b55f986860d8bc22193f8b9847d4796de5
kevin qiu <jinxiu89@163.com>
2021-09-26 14:41:02 +0800

    #路由修复 内容管理

--- app/admin/route/Content.php
+++ app/admin/route/Content.php
@@ -13,18 +13,18 @@
 use think\facade\Route;
 
 Route::group('content', function () {
-    Route::rule('/index', 'content.Index/index');
+    Route::rule('/index', 'content.Index/index')->name('content_index');
     //    产品
-    Route::rule('/product/index', 'content.Product/index');
-    Route::rule('/product/add', 'content.Product/add', 'GET|POST');
-    Route::rule('/product/edit', 'content.Product/edit', 'GET|POST');
-    Route::rule('/product/change_status', 'content.Product/changeStatus', 'GET|POST');
-    Route::rule('/product/order', 'content.Product/order', 'POST');
+    Route::rule('/product/index', 'content.Product/index')->name('content_product_index');
+    Route::rule('/product/add', 'content.Product/add', 'GET|POST')->name('content_add_product');
+    Route::rule('/product/edit', 'content.Product/edit', 'GET|POST')->name('content_edit_product');
+    Route::rule('/product/change_status', 'content.Product/changeStatus', 'GET|POST')->name('content_change_product');
+    Route::rule('/product/order', 'content.Product/order', 'POST')->name('content_order_product');
 
     //Prettiest 优质产品 首页霸屏，栏目推介位
-    Route::rule('/prettiest/index', 'content.Prettiest/index', 'GET');
-    Route::rule('/prettiest/add', 'content.Prettiest/add', 'GET|POST');
-    Route::rule('/prettiest/edit', 'content.Prettiest/edit', 'GET|POST');
-    Route::rule('/prettiest/change_status', 'content.Prettiest/changeStatus', 'GET');
-    Route::rule('/notice', 'content.Notice/notice', 'GET|POST');
+    Route::rule('/prettiest/index', 'content.Prettiest/index', 'GET')->name('content_pretties_index');
+    Route::rule('/prettiest/add', 'content.Prettiest/add', 'GET|POST')->name('content_add_pretties');
+    Route::rule('/prettiest/edit', 'content.Prettiest/edit', 'GET|POST')->name('content_edit_pretties');
+    Route::rule('/prettiest/change_status', 'content.Prettiest/changeStatus', 'GET')->name('content_change_pretties');
+    Route::rule('/notice', 'content.Notice/notice', 'GET|POST')->name('content_notice');
 });
